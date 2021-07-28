a30e21849d0bd4412d5bde87b01786fae6529d5f
Muhammad Zourdy <zhafarmuhammad12@gmail.com>
2021-07-28 14:11:44 +0700

    refactor code folder

--- 04-accordion/src/app.js
+++ 04-accordion/src/app.js
@@ -2,8 +2,6 @@ import React, { useState } from "react";
 import data from "./data";
 import SingleQuestion from "./Question";
 
-function App() {
+export const App = () => {
   return <h2>accordion project setup</h2>;
-}
-
-export default App;
+};
