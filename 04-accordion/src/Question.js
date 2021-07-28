c7bcef7503221edff26e950e40c11f898f3ff645
Muhammad Zourdy <zhafarmuhammad12@gmail.com>
2021-07-28 17:19:34 +0700

    rendering the question accordion

--- 04-accordion/src/Question.js
+++ 04-accordion/src/Question.js
@@ -1,7 +1,18 @@
-import React, { useState } from 'react';
-import { AiOutlineMinus, AiOutlinePlus } from 'react-icons/ai';
-const Question = () => {
-  return <h2>question component</h2>;
-};
+import React, { useState } from "react";
+import { AiOutlineMinus, AiOutlinePlus } from "react-icons/ai";
 
-export default Question;
+export const Question = ({ title, info }) => {
+  const [toggle, setToggle] = useState(false);
+
+  return (
+    <article className="question">
+      <header>
+        <h4>{title}</h4>
+        <button className="btn" type="button">
+          {toggle ? <AiOutlineMinus /> : <AiOutlinePlus />}
+        </button>
+      </header>
+      {toggle && <p>{info}</p>}
+    </article>
+  );
+};
--- 04-accordion/src/app.js
+++ 04-accordion/src/app.js
@@ -1,7 +1,25 @@
 import React, { useState } from "react";
 import data from "./data";
-import SingleQuestion from "./Question";
+import { Question } from "./Question";
 
 export const App = () => {
-  return <h2>accordion project setup</h2>;
+  const [questions, setQuestions] = useState(data);
+
+  return (
+    <main>
+      <div className="container">
+        <h3>questions and answers about login</h3>
+
+        <section className="info">
+          {questions.map((question) => (
+            <Question
+              title={question.title}
+              info={question.info}
+              key={question.id}
+            />
+          ))}
+        </section>
+      </div>
+    </main>
+  );
 };
