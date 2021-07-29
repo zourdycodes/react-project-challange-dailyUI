61f40665aecc79fd68b8323171d3d7eb0dbc936a
Muhammad Zourdy <zhafarmuhammad12@gmail.com>
2021-07-29 15:47:01 +0700

    rendering the menu data to the UI

--- 05-menu/src/app.js
+++ 05-menu/src/app.js
@@ -3,6 +3,23 @@ import Menu from "./Menu";
 import Categories from "./Categories";
 import items from "./data";
 
+const allCategories = ["all", ...new Set(items.map((item) => item.category))];
+
 export const App = () => {
-  return <h2>menu project setup</h2>;
+  const [menuItems, setMenuItems] = useState(items);
+  const [categories, setCategories] = useState(allCategories);
+
+  return (
+    <main>
+      <section className="menu section">
+        <div className="title">
+          <h2>Our Menu</h2>
+          <div className="underline"></div>
+        </div>
+
+        <Categories />
+        <Menu items={menuItems} />
+      </section>
+    </main>
+  );
 };
