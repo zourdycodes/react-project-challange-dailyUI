680912c874d6caf107debe6a4a90116ea9354b23
Muhammad Zourdy <zhafarmuhammad12@gmail.com>
2021-07-29 13:33:25 +0700

    refactoring and clean up folder structure

---
+++ 05-menu/src/app.js
@@ -0,0 +1,8 @@
+import React, { useState } from "react";
+import Menu from "./Menu";
+import Categories from "./Categories";
+import items from "./data";
+
+export const App = () => {
+  return <h2>menu project setup</h2>;
+};
