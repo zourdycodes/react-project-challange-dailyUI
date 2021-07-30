a73c61ae345510eefca793ac422f3bb35cca0ee6
Muhammad Zourdy <zhafarmuhammad12@gmail.com>
2021-07-30 16:32:36 +0700

    fetching the data

--- 06-tabs/src/app.js
+++ 06-tabs/src/app.js
@@ -4,7 +4,51 @@ import { FaAngleDoubleRight } from "react-icons/fa";
 const url = "https://course-api.com/react-tabs-project";
 
 export const App = () => {
-  return <h2>tabs project setup</h2>;
+  const [jobs, setJobs] = useState();
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(false);
+  const [value, setValue] = useState(0);
+
+  useEffect(() => {
+    try {
+      const fetchJobs = async () => {
+        const response = await fetch(url);
+
+        // handling error
+        if (!response.ok && response.status > 400) {
+          return setError((error) => !error);
+        }
+
+        const data = await response.json();
+
+        console.log(data);
+        setJobs(data);
+        setLoading((loading) => !loading);
+      };
+
+      fetchJobs();
+    } catch (error) {
+      throw new Error(error.message);
+    }
+  }, []);
+
+  return (
+    <main>
+      {loading ? (
+        <h1>Loading ....</h1>
+      ) : (
+        jobs?.map((jobData) => {
+          const { id, company } = jobData;
+
+          return (
+            <main key={id}>
+              <p>{company}</p>
+            </main>
+          );
+        })
+      )}
+    </main>
+  );
 };
 
 export default App;
