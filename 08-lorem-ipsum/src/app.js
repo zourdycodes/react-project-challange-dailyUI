2dc685bdb3f424b65466ab9a73bdef00ef6397d8
Muhammad Zourdy <zhafarmuhammad12@gmail.com>
2021-08-01 19:38:23 +0700

    rendering gibberish text

--- 08-lorem-ipsum/src/app.js
+++ 08-lorem-ipsum/src/app.js
@@ -2,7 +2,38 @@ import React, { useState } from "react";
 import data from "./data";
 
 export const App = () => {
-  return <h2>lorem ipsum project setup</h2>;
+  const [count, setCount] = useState(0);
+  const [text, setText] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setText(data);
+  };
+
+  return (
+    <section className="section-center">
+      <h3>a new way of lorem ipsum</h3>
+      <form onSubmit={handleSubmit} className="lorem-form">
+        <label htmlFor="amount">paragraph:</label>
+        <input
+          type="number"
+          name="amount"
+          id="amount"
+          value={count}
+          onChange={({ target }) => setCount(target.value)}
+        />
+        <button className="btn" type="submit">
+          generate
+        </button>
+      </form>
+
+      <article className="lorem-text">
+        {text?.map((item, index) => (
+          <p key={index}>{item}</p>
+        ))}
+      </article>
+    </section>
+  );
 };
 
 export default App;
