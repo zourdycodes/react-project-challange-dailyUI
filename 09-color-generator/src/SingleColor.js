7af6400107f14c0f9a87834c45ea37a75cd48ada
Muhammad Zourdy <zhafarmuhammad12@gmail.com>
2021-08-03 17:17:05 +0700

    adding copy-to-clipboard functionalities && timer animation

--- 09-color-generator/src/SingleColor.js
+++ 09-color-generator/src/SingleColor.js
@@ -7,15 +7,32 @@ const SingleColor = ({ index, rgb, weight }) => {
 
   const hex = rgbToHex(...rgb);
 
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      if (alert) {
+        setAlert(false);
+      }
+    }, 3000);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [alert]);
+
   return (
     <article
-      className={`color`}
+      className={`color ${index > 9 && "color-light"}`}
       style={{
         backgroundColor: `rgb(${rgbToString})`,
+      }}
+      onClick={() => {
+        setAlert((alert) => !alert);
+        navigator.clipboard.writeText(hex);
       }}
     >
       <p className="percent-value">{weight}%</p>
       <p className="color-value">{hex}</p>
+      {alert && <p className="alert">copied to clipboard</p>}
     </article>
   );
 };
