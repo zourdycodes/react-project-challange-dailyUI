e647d2c80707dca2d89c5cb65fb2d54bc71b6130
Muhammad Zourdy <zhafarmuhammad12@gmail.com>
2021-08-03 14:58:51 +0700

    structuring the project and create the skeleton of the app

--- 09-color-generator/src/app.js
+++ 09-color-generator/src/app.js
@@ -4,5 +4,34 @@ import SingleColor from "./SingleColor";
 import Values from "values.js";
 
 export const App = () => {
-  return <h2>color generator project</h2>;
+  const [color, setColor] = useState("");
+  const [error, setError] = useState(false);
+  const [list, setList] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("hello");
+  };
+
+  return (
+    <>
+      <section className="container">
+        <h3>Color Generator</h3>
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            placeholder="#f15025"
+            value={color}
+            onChange={({ target }) => setColor(target.value)}
+          />
+          <button className="btn" type="submit">
+            submit
+          </button>
+        </form>
+      </section>
+      <section className="colors">
+        <h4>list goes here</h4>
+      </section>
+    </>
+  );
 };
