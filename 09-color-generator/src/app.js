42374e9412935adb875291d0cc578319cd649462
Muhammad Zourdy <zhafarmuhammad12@gmail.com>
2021-08-03 14:36:59 +0700

    installing dependencies --> refactoring boilerplate

---
+++ 09-color-generator/src/app.js
@@ -0,0 +1,8 @@
+import React, { useState } from "react";
+import SingleColor from "./SingleColor";
+
+import Values from "values.js";
+
+export const App = () => {
+  return <h2>color generator project</h2>;
+};
