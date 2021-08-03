a1b6eb4918a753b1dc6c6d4d9a1e92dc9b4f137a
Muhammad Zourdy <zhafarmuhammad12@gmail.com>
2021-08-03 17:19:48 +0700

    default color theme

--- 09-color-generator/src/app.js
+++ 09-color-generator/src/app.js
@@ -6,7 +6,7 @@ import Values from "values.js";
 export const App = () => {
   const [color, setColor] = useState("");
   const [error, setError] = useState(false);
-  const [list, setList] = useState([]);
+  const [list, setList] = useState(new Values("#f15025").all(10));
 
   const handleSubmit = (e) => {
     e.preventDefault();
