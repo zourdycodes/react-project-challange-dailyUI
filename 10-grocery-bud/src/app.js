7bfd76fc646db4e5ff6b7616ace1a795905f2a54
Muhammad Zourdy <zhafarmuhammad12@gmail.com>
2021-08-04 10:34:44 +0700

    setup the Alert components

--- 10-grocery-bud/src/app.js
+++ 10-grocery-bud/src/app.js
@@ -7,7 +7,11 @@ export const App = () => {
   const [list, setList] = useState([]);
   const [isEditing, setIsEditing] = useState(false);
   const [editId, setEditId] = useState(null);
-  const [alert, setAlert] = useState({ show: false, msg: "", type: "" });
+  const [alert, setAlert] = useState({
+    show: true,
+    msg: "Hello",
+    type: "success",
+  });
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -35,7 +39,7 @@ export const App = () => {
   return (
     <section className="section-center">
       <form className="grocery-form" onSubmit={handleSubmit}>
-        {alert.show && <Alert />}
+        {alert.show && <Alert {...alert} />}
         <h3>grocery buddy</h3>
         <div className="form-control">
           <input
