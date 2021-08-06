53005f965b2626f5b6f51f2b0abc9a6223989a68
Muhammad Zourdy <zhafarmuhammad12@gmail.com>
2021-08-06 07:27:59 +0700

    rendering links and place it in rendered component

--- 11-navbar/src/Navbar.js
+++ 11-navbar/src/Navbar.js
@@ -1,10 +1,31 @@
-import React, { useState, useRef, useEffect } from 'react'
-import { FaBars, FaTwitter } from 'react-icons/fa'
-import { links, social } from './data'
-import logo from './logo.svg'
+import React, { useState, useRef, useEffect } from "react";
+import { FaBars, FaTwitter } from "react-icons/fa";
+import { links, social } from "./data";
+import logo from "./logo.svg";
 
 const Navbar = () => {
-  return <h4>navbar</h4>
-}
+  return (
+    <nav>
+      <div className="nav-center">
+        <div className="nav-header">
+          <img src={logo} alt="logo-addict-coder" />
+          <button className="nav-toggle">
+            <FaBars />
+          </button>
+        </div>
+        <div className="links-container show-container">
+          <ul className="links">
+            {links?.map((link) => (
+              <li key={link.id}>
+                <a href={link.url}>{link.text}</a>
+              </li>
+            ))}
+          </ul>
+        </div>
+        <ul className="social-icons"></ul>
+      </div>
+    </nav>
+  );
+};
 
-export default Navbar
+export default Navbar;
--- 11-navbar/src/app.js
+++ 11-navbar/src/app.js
@@ -4,7 +4,7 @@ import Navbar from "./Navbar";
 export const App = () => {
   return (
     <>
-      <h2>navbar project setup</h2>
+      <Navbar />
     </>
   );
 };
