eb6d6cdceb20e69cebdf894fd69b19dfc15ab629
Muhammad Zourdy <zhafarmuhammad12@gmail.com>
2021-08-06 16:50:50 +0700

    installing dependencies and refactor boilerplate

---
+++ 12-sidebar-modal/src/app.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Modal from "./Modal";
+import Sidebar from "./Sidebar";
+import Home from "./Home";
+
+export const App = () => {
+  return (
+    <>
+      <h2>modal-sidebar project setup</h2>
+    </>
+  );
+};
