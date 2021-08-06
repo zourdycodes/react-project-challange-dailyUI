dc17223432ae7273759c5a0f13fbf8c22f5c452d
Muhammad Zourdy <zhafarmuhammad12@gmail.com>
2021-08-06 17:34:26 +0700

    setting up the context api

--- 12-sidebar-modal/src/app.js
+++ 12-sidebar-modal/src/app.js
@@ -3,12 +3,14 @@ import Modal from "./Modal";
 import Sidebar from "./Sidebar";
 import Home from "./Home";
 
+import { ContextProvider } from "./context";
+
 export const App = () => {
   return (
-    <>
+    <ContextProvider>
       <Home />
       <Modal />
       <Sidebar />
-    </>
+    </ContextProvider>
   );
 };
--- 12-sidebar-modal/src/context.js
+++ 12-sidebar-modal/src/context.js
@@ -1,1 +1,21 @@
-import React, { useState, useContext } from 'react'
+import React, { useState, useContext, createContext } from "react";
+
+export const ModalContext = createContext();
+
+export const ContextProvider = (props) => {
+  const [showSidebar, setShowSidebar] = useState(false);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  return (
+    <ModalContext.Provider
+      value={{
+        showSidebar,
+        setShowSidebar,
+        isModalOpen,
+        setIsModalOpen,
+      }}
+    >
+      {props.children}
+    </ModalContext.Provider>
+  );
+};
