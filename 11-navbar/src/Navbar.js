78abdbf62c51c2873c9a4b139265059f92221238
Muhammad Zourdy <zhafarmuhammad12@gmail.com>
2021-08-06 07:55:15 +0700

    create dynamic link size

--- 11-navbar/src/Navbar.js
+++ 11-navbar/src/Navbar.js
@@ -1,10 +1,23 @@
 import React, { useState, useRef, useEffect } from "react";
-import { FaBars, FaTwitter } from "react-icons/fa";
+import { FaBars } from "react-icons/fa";
 import { links, social } from "./data";
 import logo from "./logo.svg";
 
 const Navbar = () => {
   const [toggleShow, setToggleShow] = useState(false);
+
+  const refContainer = useRef(null);
+  const refLinks = useRef(null);
+
+  useEffect(() => {
+    const linksHeight = refLinks.current.getBoundingClientRect().height;
+
+    if (toggleShow) {
+      refContainer.current.style.height = `${linksHeight}px`;
+    } else {
+      refContainer.current.style.height = `0px`;
+    }
+  }, [toggleShow]);
 
   return (
     <nav>
@@ -18,8 +31,11 @@ const Navbar = () => {
             <FaBars />
           </button>
         </div>
-        <div className={`links-container ${toggleShow && "show-container"}`}>
-          <ul className="links">
+        <div
+          className={`links-container ${toggleShow && "show-container"}`}
+          ref={refContainer}
+        >
+          <ul className="links" ref={refLinks}>
             {links?.map((link) => (
               <li key={link.id}>
                 <a href={link.url}>{link.text}</a>
