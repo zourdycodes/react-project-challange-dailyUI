d266637e9653c9cfbcd5bbc4d354bd05efab8f70
Muhammad Zourdy <zhafarmuhammad12@gmail.com>
2021-08-06 17:40:13 +0700

    handling the state context

--- 12-sidebar-modal/src/Home.js
+++ 12-sidebar-modal/src/Home.js
@@ -1,13 +1,22 @@
 import React, { useContext } from "react";
 import { FaBars } from "react-icons/fa";
+import { ModalContext } from "./context";
 
 const Home = () => {
+  const { showSidebar, setShowSidebar, setIsModalOpen } =
+    useContext(ModalContext);
+
   return (
     <main>
       <button className="sidebar-toggle">
         <FaBars />
       </button>
-      <button className="btn">show modal</button>
+      <button
+        className="btn"
+        onClick={() => setIsModalOpen((isModalOpen) => !isModalOpen)}
+      >
+        show modal
+      </button>
     </main>
   );
 };
--- 12-sidebar-modal/src/Modal.js
+++ 12-sidebar-modal/src/Modal.js
@@ -1,12 +1,18 @@
-import React from "react";
+import React, { useContext } from "react";
 import { FaTimes } from "react-icons/fa";
+import { ModalContext } from "./context";
 
 const Modal = () => {
+  const { isModalOpen, setIsModalOpen } = useContext(ModalContext);
+
   return (
-    <div className={`modal-overlay`}>
+    <div className={`modal-overlay ${isModalOpen && "show-modal"}`}>
       <div className="modal-container">
         <h3>modal content</h3>
-        <button className="close-modal-btn">
+        <button
+          className="close-modal-btn"
+          onClick={() => setIsModalOpen(false)}
+        >
           <FaTimes />
         </button>
       </div>
