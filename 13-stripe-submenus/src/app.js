21c5af6db51b777a5ff4b3223ed619107c2c8ce7
Muhammad Zourdy <zhafarmuhammad12@gmail.com>
2021-08-08 05:49:24 +0700

    installing dependencies and refactoring boilerplate

---
+++ 13-stripe-submenus/src/app.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Navbar from "./Navbar";
+import Hero from "./Hero";
+import Sidebar from "./Sidebar";
+import Submenu from "./Submenu";
+
+export const App = () => {
+  return (
+    <>
+      <h2>stripe submenus setup</h2>
+    </>
+  );
+};
--- 13-stripe-submenus/src/index.js
+++ 13-stripe-submenus/src/index.js
@@ -1,10 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import './index.css'
-import App from './App'
-ReactDOM.render(
+import React from "react";
+import { render } from "react-dom";
+import "./index.css";
+import { App } from "./app";
+
+render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
-)
+  document.getElementById("root")
+);
