2204c0a87ff547017e39161a6c7eaebb6fe95143
Muhammad Zourdy <zhafarmuhammad12@gmail.com>
2021-08-08 08:15:56 +0700

    finishing create the sidebar component

--- 13-stripe-submenus/src/Sidebar.js
+++ 13-stripe-submenus/src/Sidebar.js
@@ -1,9 +1,49 @@
-import React from 'react'
-import { FaTimes } from 'react-icons/fa'
-import sublinks from './data'
+import React from "react";
+import { FaTimes } from "react-icons/fa";
+import { subLinks } from "./data";
+
+import { useGlobalContext } from "./context";
 
 const Sidebar = () => {
-  return <h2>sidebar component</h2>
-}
+  const { isSidebarOpen, closeSidebar } = useGlobalContext();
+  return (
+    <aside
+      className={`${
+        isSidebarOpen ? "sidebar-wrapper show" : "sidebar-wrapper"
+      }`}
+    >
+      <div className="sidebar">
+        <button onClick={closeSidebar} className="close-btn">
+          <FaTimes />
+        </button>
 
-export default Sidebar
+        <div className="sidebar-links">
+          {subLinks?.map((item) => {
+            const { id, page, links } = item;
+
+            return (
+              <article key={id}>
+                <h4>{page}</h4>
+                <div className="sidebar-sublinks">
+                  {links.map((link, index) => {
+                    const { label, icon, url } = link;
+
+                    return (
+                      <>
+                        <a href={url} key={index}>
+                          {icon} {label}
+                        </a>
+                      </>
+                    );
+                  })}
+                </div>
+              </article>
+            );
+          })}
+        </div>
+      </div>
+    </aside>
+  );
+};
+
+export default Sidebar;
