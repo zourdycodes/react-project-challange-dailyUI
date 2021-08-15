ac844f5f33105436f12c44eacc98bcd485a3f7b7
Muhammad Zourdy <zhafarmuhammad12@gmail.com>
2021-08-15 17:04:49 +0700

    handling with total amount of price

--- 14-cart/src/reducer.js
+++ 14-cart/src/reducer.js
@@ -60,6 +60,30 @@ export const reducer = (state, action) => {
         ...state,
         cart: newCart,
       };
+    case "GET_TOTALS":
+      let { total, amount } = state.cart.reduce(
+        (cartTotal, cartItem) => {
+          const { price, amount } = cartItem;
+          const itemTotal = price * amount;
+
+          cartTotal.total += itemTotal;
+          cartTotal.amount += amount;
+
+          return cartTotal;
+        },
+        {
+          total: 0,
+          amount: 0,
+        }
+      );
+
+      total = parseFloat(total.toFixed(2));
+
+      return {
+        ...state,
+        total,
+        amount,
+      };
 
     default:
       return {
