0269d37e7d391c19d985f56ef2eaf0c8b72d049c
Muhammad Zourdy <zhafarmuhammad12@gmail.com>
2021-08-16 12:06:06 +0700

    installing missing packages

--- 15-cocktails/src/index.js
+++ 15-cocktails/src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import { render } from "react-dom";
 import "./index.css";
 import { App } from "./app";
-import { AppProvider } from "./context";
+import { AppProvider } from "./context/context";
 
 render(
   <React.StrictMode>
@@ -12,7 +12,3 @@ render(
   </React.StrictMode>,
   document.getElementById("root")
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
