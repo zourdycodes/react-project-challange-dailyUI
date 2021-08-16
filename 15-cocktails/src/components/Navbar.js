c92a44ac9e18ae9397a0188574f1a5eec04943e5
Muhammad Zourdy <zhafarmuhammad12@gmail.com>
2021-08-16 15:17:23 +0700

    setup about and error page

--- 15-cocktails/src/components/Navbar.js
+++ 15-cocktails/src/components/Navbar.js
@@ -1,12 +1,26 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import logo from "../logo.svg";
+import * as PATH from "../constant/routes";
 
 const Navbar = () => {
   return (
-    <div>
-      <h2>navbar component</h2>
-    </div>
+    <nav className="navbar">
+      <div className="nav-center">
+        <Link to={PATH.HOME}>
+          <img src={logo} alt="cocktail db logo" className="logo" />
+        </Link>
+
+        <ul className="nav-links">
+          <li>
+            <Link to={PATH.HOME}>Home</Link>
+          </li>
+          <li>
+            <Link to={PATH.ABOUT}>About</Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
   );
 };
 
--- 15-cocktails/src/pages/About.js
+++ 15-cocktails/src/pages/About.js
@@ -1,11 +1,23 @@
-import React from 'react'
+import React from "react";
 
 const About = () => {
   return (
-    <div>
-      <h2>about page</h2>
-    </div>
-  )
-}
+    <section className="section about-section">
+      <h1 className="section-title">about page</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit quae
+        porro magni accusantium earum consectetur iste, sint suscipit voluptate
+        repellendus ipsam! Deleniti voluptatibus vitae doloribus neque illum ex
+        et provident illo quasi, aliquam commodi consectetur earum. Ab quaerat
+        similique eius!
+      </p>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Mollitia ut
+        voluptates molestiae doloribus quam nemo. Pariatur magnam quo
+        consequuntur maiores.
+      </p>
+    </section>
+  );
+};
 
-export default About
+export default About;
