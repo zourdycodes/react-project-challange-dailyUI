9ea315041fc03b2f1edcd3bd5a9d7a9905ccd3b5
Muhammad Zourdy <zhafarmuhammad12@gmail.com>
2021-08-16 12:02:04 +0700

    installing the the dependecies

--- 15-cocktails/src/index.js
+++ 15-cocktails/src/index.js
@@ -1,10 +1,10 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { render } from "react-dom";
 import "./index.css";
 import { App } from "./app";
 import { AppProvider } from "./context";
 
-ReactDOM.render(
+render(
   <React.StrictMode>
     <AppProvider>
       <App />
