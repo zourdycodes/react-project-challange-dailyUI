a7a10298004a5fc2f79a7b0dee5faef8a3065dc0
Muhammad Zourdy <zhafarmuhammad12@gmail.com>
2021-08-17 16:24:12 +0700

    setup the workbranch

--- 16-markdown-preview/src/app.js
+++ 16-markdown-preview/src/app.js
@@ -2,5 +2,12 @@ import React, { useState } from "react";
 import ReactMarkdown from "react-markdown";
 
 export const App = () => {
-  return <h2>markdown preview starter</h2>;
+  const [markdown, setMarkdown] = useState("## markdown preview");
+  return (
+    <main>
+      <section className="markdown">
+        <textarea className="input"></textarea>
+      </section>
+    </main>
+  );
 };
