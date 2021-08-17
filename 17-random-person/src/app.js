604c2d0766bc50349f40e15ed67d388a6980f91a
Muhammad Zourdy <zhafarmuhammad12@gmail.com>
2021-08-17 20:45:32 +0700

    setup the project

--- 17-random-person/src/app.js
+++ 17-random-person/src/app.js
@@ -12,5 +12,28 @@ const url = "https://randomuser.me/api/";
 const defaultImage = "https://randomuser.me/api/portraits/men/75.jpg";
 
 export const App = () => {
-  return <h2>random user starter</h2>;
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(false);
+  const [title, setTitle] = useState("name");
+  const [person, setPerson] = useState(null);
+  const [value, setValue] = useState("random name");
+
+  const handleValue = (e) => {};
+
+  return (
+    <main>
+      <div className="block bcg-black"></div>
+      <div className="block">
+        <div className="container">
+          <img
+            src={(person && person.image) || defaultImage}
+            alt="random user"
+            className="user-img"
+          />
+          <p className="user-title">my {title} is</p>
+          <p className='user-value'>{value}</p>
+        </div>
+      </div>
+    </main>
+  );
 };
