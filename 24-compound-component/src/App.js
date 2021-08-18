c09f248e8768e7606aa4401a0eec659bb65609ff
Muhammad Zourdy <zhafarmuhammad12@gmail.com>
2021-08-18 19:11:35 +0700

    clean up, pushing to codebase

--- 24-compound-component/src/App.js
+++ 24-compound-component/src/App.js
@@ -1,10 +1,20 @@
 import "./App.scss";
+import { Card } from "./components/Card";
 import movies from "./data";
 
 export const App = () => {
   return (
-    <div className="App">
-      <h1>response</h1>
-    </div>
+    <main>
+      {movies.map((movie) => (
+        <Card classes="mr" key={`${movie.id}`}>
+          <Card.Image src={movie.image} alt={movie.title} />
+          <Card.Body>
+            <Card.Title>{movie.title}</Card.Title>
+            <Card.Text>{movie.desc}</Card.Text>
+            <Card.Button>{movie.ctaText}</Card.Button>
+          </Card.Body>
+        </Card>
+      ))}
+    </main>
   );
 };
