7aa3b83d567a89e37cc7cb5baf0fb1fe878cb962
Muhammad Zourdy <zhafarmuhammad12@gmail.com>
2021-08-19 13:57:17 +0700

    setup the pagination logic functionality

--- 18-pagination/src/useFetch.js
+++ 18-pagination/src/useFetch.js
@@ -10,6 +10,7 @@ export const useFetch = () => {
   const getProducts = async () => {
     const response = await fetch(url);
     const data = await response.json();
+    paginate(data)
     setData(data);
     setLoading(false);
   };
--- 18-pagination/src/utils.js
+++ 18-pagination/src/utils.js
@@ -1,1 +1,11 @@
-export const paginate = () => {};
+export const paginate = (followers) => {
+  const itemPerPages = 9;
+  const pages = Math.ceil(followers.length / itemPerPages);
+
+  const newFollowers = Array.from({ length: pages }, (_, index) => {
+    const start = index * itemPerPages;
+    return followers.slice(start, start + itemPerPages); // (0, 9) , (9, 18) etc to 99
+  });
+
+  return newFollowers;
+};
