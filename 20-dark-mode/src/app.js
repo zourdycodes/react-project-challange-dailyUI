b721fbabc77500b6c1cc528265cf876bf868512c
Muhammad Zourdy <zhafarmuhammad12@gmail.com>
2021-08-21 13:52:03 +0700

    finishing project

--- 20-dark-mode/src/app.js
+++ 20-dark-mode/src/app.js
@@ -3,5 +3,36 @@ import data from "./data";
 import Article from "./Article";
 
 export const App = () => {
-  return <h2>dark mode starter</h2>;
+  const [theme, setTheme] = useState("light-theme");
+
+  const toggleTheme = () => {
+    if (theme === "light-theme") {
+      setTheme("dark-theme");
+    } else {
+      setTheme("light-theme");
+    }
+  };
+
+  useEffect(() => {
+    document.documentElement.className = theme;
+  }, [theme]);
+
+  return (
+    <main>
+      <nav>
+        <div className="nav-center">
+          <h1>overreacted</h1>
+          <button className="btn" onClick={toggleTheme}>
+            toggle
+          </button>
+        </div>
+      </nav>
+
+      <section className="articles">
+        {data?.map((data, index) => {
+          return <Article key={index} {...data} />;
+        })}
+      </section>
+    </main>
+  );
 };
