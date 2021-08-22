3ab40dc7b906a04539605b45e09a05f0b7b810f2
Muhammad Zourdy <zhafarmuhammad12@gmail.com>
2021-08-22 09:20:01 +0700

    handling search form

--- 21-movie-db/src/SearchForm.js
+++ 21-movie-db/src/SearchForm.js
@@ -2,7 +2,19 @@ import React from "react";
 import { useGlobalContext } from "./context";
 
 const SearchForm = () => {
-  return <h2>search component</h2>;
+  const { query, setQuery, error } = useGlobalContext();
+
+  return (
+    <form onSubmit={(e) => e.preventDefault()} className="search-form">
+      <h2>Search Movies</h2>
+      <input
+        type="text"
+        className="form-input"
+        value={query}
+        onChange={({ target }) => setQuery(target.value)}
+      />
+    </form>
+  );
 };
 
 export default SearchForm;
